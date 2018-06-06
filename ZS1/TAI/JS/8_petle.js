4c5ce408cabf9717dffce662b6d4e81ff33f2c1c
Sandra Korcz <35773058+sandrakorcz@users.noreply.github.com>
2018-06-06 17:47:54 +0200

    Add files via upload

--- ZS1/TAI/JS/8_petle.js
+++ ZS1/TAI/JS/8_petle.js
@@ -1,14 +1,14 @@
 
   /*for (var i=0; i<=100; i++){
     document.write(i + "<br>");
-  }*/ 
-    
+  }*/
+
    /* for (var i=5; i<=100; i+=5){
     document.write(i + "<br>");
   }*/
-    
+
 //ZADANIE 1 Wyświtl liczby parzyste z przedziału <3;20>
-    
+
     /*for (var i=3; i<=20; i+=2){
       if (i%2 == 0)
       document.write(i + " ");
@@ -23,7 +23,7 @@
 
 //ZADANIE 3 Użytkownik podaje z klawiatury wartość poczatkowa i końcową przedziału. Wybiera za pomocą przycisku radio czy dane maja być wyświetlane rosnąco czy malejąco. Wyświetl domyślnie dane posortowane rosnąco. Wybierjąc przycisk radio będzie modyfikowane sortowanie.
 //document.write('<div id="wyswietl">');
-var wyswietl = document.getElementById('wyswietl');  
+var wyswietl = document.getElementById('wyswietl');
 
 document.write('<input type="radio" checked name="sort" id="r" >rosnąco <br>');
 document.write('<input type="radio" name="sort" id="m" >malejąco <br>');
